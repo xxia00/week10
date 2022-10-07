17e2fc1c9fcd82b3a5c06bf21694fada732905c5
xxia00 <112386172+xxia00@users.noreply.github.com>
2022-10-07 11:08:41 +0800

    Add files via upload
    vegalite

---
+++ vega_lite_vis.js
@@ -0,0 +1,16 @@
+var vg_1 = "scatterplot.json";
+vegaEmbed("#scatterplot", vg_1).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+
+var vg_1 = "map_happiness.vg.json";
+vegaEmbed("#map_happiness", vg_1).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+
+var vg_1 = "radial.json";
+vegaEmbed("#radial", vg_1).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
